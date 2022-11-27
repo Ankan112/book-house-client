258f0a86d2f73df3fcafd09fe4d9545267b31b49
Ankan112 <ankanhalder112@gmail.com>
2022-11-27 23:54:07 +0600

    delete buyer

--- src/Pages/AllBuyer/AllBuyer.js
+++ src/Pages/AllBuyer/AllBuyer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
 const AllBuyer = () => {
     const [buyerData, setBuyerData] = useState([])
@@ -7,9 +8,46 @@ const AllBuyer = () => {
             .then(res => res.json())
             .then(data => setBuyerData(data))
     }, [])
+    const handleDelete = id => {
+        const agree = window.confirm('Are you sure you want to delete this account?')
+        if (agree) {
+            fetch(`http://localhost:5000/users/buyer/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        toast.success('Account Delete Successful')
+                    }
+                })
+        }
+    }
     return (
-        <div>
-            <h1>all buyers {buyerData.length}</h1>
+        <div className="overflow-x-auto">
+            <table className="table w-full">
+
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Delete Button</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        buyerData.map((data, i) => <tr key={data._id}>
+                            <th>{i + 1}</th>
+                            <td>{data.name}</td>
+                            <td>{data.email}</td>
+                            <td><button onClick={() => handleDelete(data._id)} className="btn btn-error">Delete</button></td>
+                        </tr>)
+                    }
+
+
+                </tbody>
+            </table>
         </div>
     );
 };
